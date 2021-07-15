85bc434d887b28144497fd76a04c9bf8b54a3af7
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2021-07-15 12:04:10 -0700

    updat customer dashboard

--- resources/views/layouts/backend/components/header/topheader.blade.php
+++ resources/views/layouts/backend/components/header/topheader.blade.php
@@ -121,13 +121,22 @@
                 <div class="dropdown d-flex">
                    <a class="nav-link icon d-none d-md-flex btn btn-default btn-icon ml-1" data-toggle="dropdown"><i class="fa fa-user"></i></a>
                    <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                      <a class="dropdown-item" href="page-profile.html"><i class="dropdown-icon fe fe-user"></i> Profile</a>
-                      <a class="dropdown-item" href="app-setting.html"><i class="dropdown-icon fe fe-settings"></i> Settings</a>
+                      <a class="dropdown-item" href="javascript:void(0)"><i class="dropdown-icon fe fe-user"></i> Profile</a>
+                      <a class="dropdown-item" href="javascript:void(0)"><i class="dropdown-icon fe fe-settings"></i> Settings</a>
                       <a class="dropdown-item" href="javascript:void(0)"><span class="float-right"><span class="badge badge-primary">6</span></span><i class="dropdown-icon fe fe-mail"></i> Inbox</a>
                       <a class="dropdown-item" href="javascript:void(0)"><i class="dropdown-icon fe fe-send"></i> Message</a>
                       <div class="dropdown-divider"></div>
                       <a class="dropdown-item" href="javascript:void(0)"><i class="dropdown-icon fe fe-help-circle"></i> Need help?</a>
-                      <a class="dropdown-item" href="login.html"><i class="dropdown-icon fe fe-log-out"></i> Sign out</a>
+                      <a class="dropdown-item" href="{{ route('logout') }}"
+                      onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                      <i class="dropdown-icon fe fe-log-out"></i> Sign out
+                   </a>
+
+                   <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                       @csrf
+                   </form>
+
                    </div>
                 </div>
              </div>
